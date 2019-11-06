e91713c99902b28123dcc69e86e389584589f222
Cédric Maria-Sube <cedric@maria-sube.com>
2019-11-06 17:54:57 +0000

    Handles an empty data set.

--- src/main/java/com/domain/spider/map/DomainSpiderMapGenerator.java
+++ src/main/java/com/domain/spider/map/DomainSpiderMapGenerator.java
@@ -1,12 +1,77 @@
 package com.domain.spider.map;
 
 import java.io.File;
+import java.io.IOException;
+
+import lombok.extern.log4j.Log4j2;
 
 import com.domain.spider.map.image.ImageFileType;
 
+@Log4j2
 public class DomainSpiderMapGenerator {
     
-    public File draw(String sourceDataFilePath, String imageDestinationFilePath, ImageFileType imageFileType) {
-        return null;
+    public File generate(String sourceDataFilePath, String imageDestinationFilePath, ImageFileType imageFileType) {
+        if (validateInputs(sourceDataFilePath, imageDestinationFilePath, imageFileType)) {
+            File sourceDataFile = new File(sourceDataFilePath);
+            
+            File imageDestinationFile = initlizeImageDestinationFile(imageDestinationFilePath);            
+            
+            switch (imageFileType) {
+                case JPG :
+                    return drawJpg(sourceDataFile, imageDestinationFile);
+                case PNG :
+                    return drawPng(sourceDataFile, imageDestinationFile);
+                case SVG :
+                    return drawSvg(sourceDataFile, imageDestinationFile);
+                default :
+                    return null;
+            }
+        } else {
+            return null;
+        }
+    }
+    
+    private File initlizeImageDestinationFile(String imageDestinationFilePath) {
+        File imageDestinationFile = new File(imageDestinationFilePath);
+        
+        if (imageDestinationFile != null) {
+            if (!imageDestinationFile.getParentFile().exists()) {
+                if (!imageDestinationFile.getParentFile().mkdirs()) {
+                    log.error("Cannot create directory : " + imageDestinationFile.getParentFile().getAbsolutePath());   
+                }
+            }
+            
+            if (!imageDestinationFile.exists()) {
+                try {
+                    imageDestinationFile.createNewFile();
+                } catch (IOException ioe) {
+                    log.error("Cannot create file : " + imageDestinationFilePath, ioe);   
+                }
+            }
+        }
+        
+        return imageDestinationFile;
+    }
+    
+    private File drawJpg(File sourceDataFile, File imageDestinationFile) {
+        return imageDestinationFile;
+    }
+    
+    private File drawPng(File sourceDataFile, File imageDestinationFile) {
+        return imageDestinationFile;
+    }
+    
+    private File drawSvg(File sourceDataFile, File imageDestinationFile) {
+        return imageDestinationFile;
+    }
+    
+    private boolean validateInputs(String sourceDataFilePath, String imageDestinationFilePath, ImageFileType imageFileType) {
+        if ((sourceDataFilePath == null)
+            || (imageDestinationFilePath == null)
+            || (imageFileType == null)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- src/test/java/com/domain/spider/map/DomainSpiderMapGeneratorTest.java
+++ src/test/java/com/domain/spider/map/DomainSpiderMapGeneratorTest.java
@@ -1,9 +1,13 @@
 package com.domain.spider.map;
 
 import java.io.File;
+import java.io.IOException;
 
 import lombok.extern.log4j.Log4j2;
 
+import org.apache.commons.io.FileUtils;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,8 +17,33 @@ import com.domain.spider.map.image.ImageFileType;
 @Log4j2
 public class DomainSpiderMapGeneratorTest {
     
+    private static final String TARGET_TEST_PATH = "./target/test/images/";
+    
+    protected File testRootDirectoryFile;
+    
+    @Before
+    public void setUp() throws IOException {
+        testRootDirectoryFile = new File(TARGET_TEST_PATH);
+        
+        cleanTestFileDirectory();
+        
+        if (!testRootDirectoryFile.exists()) {
+            testRootDirectoryFile.mkdir();
+        }
+    }
+    
+    protected void cleanTestFileDirectory() throws IOException {
+        try {
+            if (testRootDirectoryFile.exists()) {
+                FileUtils.deleteDirectory(testRootDirectoryFile);
+            }
+        } catch (IOException ioe) {
+            throw new IOException("Unable to delete : " + testRootDirectoryFile);
+        }
+    }
+    
     @Test
-    public void drawGivenNullNullNullThenNull() {
+    public void generateGivenNullNullNullThenNull() {
         // Given
         String sourceDataFilePath = null;
         String imageDestinationFilePath = null;
@@ -23,9 +52,27 @@ public class DomainSpiderMapGeneratorTest {
         DomainSpiderMapGenerator domainSpiderMapGenerator = new DomainSpiderMapGenerator();
         
         // When
-        File imageDestinationFile = domainSpiderMapGenerator.draw(sourceDataFilePath, imageDestinationFilePath, imageFileType);
+        File imageDestinationFile = domainSpiderMapGenerator.generate(sourceDataFilePath, imageDestinationFilePath, imageFileType);
         
         // Then
         assertThat(imageDestinationFile).isNull();
+    }
+        
+    @Test
+    public void generateGivenEmptyFileAndDestinationFileAndJpgThenEmptyFile() {
+        // Given
+        String sourceDataFilePath = "./EmptyTest.json";
+        String imageDestinationFilePath = TARGET_TEST_PATH + "EmptyTestResult.jpg";
+        ImageFileType imageFileType = ImageFileType.JPG;
+        
+        DomainSpiderMapGenerator domainSpiderMapGenerator = new DomainSpiderMapGenerator();
+        
+        // When
+        File imageDestinationFile = domainSpiderMapGenerator.generate(sourceDataFilePath, imageDestinationFilePath, imageFileType);
+        log.debug("imageDestinationFile : " + imageDestinationFile.getAbsolutePath());
+        
+        // Then
+        assertThat(imageDestinationFile).isNotNull();
+        assertThat(imageDestinationFile.exists()).isTrue();
     }
 }
